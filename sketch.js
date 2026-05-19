8a8fa903c60c2cba751537b9b2849278f6ecb84a
kamyarartin-wq <kamyarartin@gmail.com>
2026-05-18 22:25:39 -0600

    Added more images and draw player.
    So now draw player function should be able to draw the player if it is called.

--- sketch.js
+++ sketch.js
@@ -16,6 +16,7 @@ function preload() {
   imgs[LEAVES] = loadImage('leafs.png');
   imgs[PLANK] = loadImage('plank.png');
   imgs[STICK] = loadImage('stick.png');
+  imgs[WOOD_PICK]  = loadImage('wood-pickaxe.png'); 
   imgs[STONE_PICK] = loadImage('stone-pickaxe.png');
   imgs[IRON_ORE] = loadImage('iron-ore.png');
   imgs[IRON_PICK] = loadImage('iron_pickaxe.png');
@@ -32,4 +33,21 @@ function setup() {
 
 function draw() {
   background("red");
+}
+
+function drawPlayer() {
+  push();
+  translate(player.x + player.w / 2, player.y + player.h / 2);
+  if (player.facing === -1) {
+    scale(-1, 1);
+  }
+  else if (Math.abs(player.vx) > 0.3) {
+    if (frameCount % 30 < 15) {
+      img = playerImgs.walking;
+    }
+    else {
+      img = playerImgs.standing;
+    }
+  }
+  pop();
 }
